03825d9257b545a6b9baa5d92f5cd1acff7e8aec
julia-boop <crdr.jc@gmail.com>
2021-07-09 12:35:51 -0300

    links

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,16 @@
 window.addEventListener('load', () => {
     let arg = document.querySelector('#header-arg')
     let eu =  document.querySelector('#header-eu')
+    let logo = document.querySelector('.logo')
 
     if(window.innerWidth <= 760){
         arg.innerHTML = 'shop arg'
         eu.innerHTML = 'shop eu'
+        logo.innerHTML = '<img src="/img/text.png" alt="">'
     }
     if(window.innerWidth > 760) {
         arg.innerHTML = 'shop argentina'
         eu.innerHTML = 'shop europe'
+        logo.innerHTML = '<img src="/img/logo1.png" alt="">'
     }
 })
