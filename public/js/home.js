3cf1f721bbc06b73b31cc3461bdb3eb4b91fe44c
julia-boop <crdr.jc@gmail.com>
2021-09-02 13:08:50 -0300

    correccion de carga del js

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,13 @@
-window.addEventListener('load', () => {
+//document.addEventListener('DOMContentLoaded', () => {
     let arg = document.querySelector('#header-arg')
     let int =  document.querySelector('#header-int')
+    let liArg =  document.querySelector('#header-li-arg')
+    let liInt =  document.querySelector('#header-li-int')
     let logo = document.querySelector('.logo')
 
     if(window.innerWidth <= 760){
+        liArg.style.width = '50%'
+        liInt.style.width = '50%'
         arg.innerHTML = 'Shop <br> Argentina'
         int.innerHTML = 'Shop <br> Internacional'
         logo.innerHTML = '<img src="/img/logoAlta.png" alt="">'
@@ -13,4 +17,4 @@ window.addEventListener('load', () => {
         int.innerHTML = 'Shop Internacional'
         logo.innerHTML = '<img src="/img/logo1.png" alt="">'
     }
-})
+//})
