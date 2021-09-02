97a088253b3e7e8dd43d4084070f40fbbb81dbef
julia-boop <crdr.jc@gmail.com>
2021-09-02 13:16:47 -0300

    prueba con window load

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     let arg = document.querySelector('#header-arg')
     let int =  document.querySelector('#header-int')
     let liArg =  document.querySelector('#header-li-arg')
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let logo = document.querySelector('.logo')
 
     if(window.innerWidth <= 760){
+        console.log(liArg)
+        logo.innerHTML = '<img src="/img/logoAlta.png" alt="">'
         liArg.style.width = '50%'
         liInt.style.width = '50%'
         arg.innerHTML = 'Shop <br> Argentina'
         int.innerHTML = 'Shop <br> Internacional'
-        logo.innerHTML = '<img src="/img/logoAlta.png" alt="">'
-    }
-    if(window.innerWidth > 760) {
+    } else if(window.innerWidth > 760) {
+        logo.innerHTML = '<img src="/img/logo1.png" alt="">'
         arg.innerHTML = 'Shop Argentina'
         int.innerHTML = 'Shop Internacional'
-        logo.innerHTML = '<img src="/img/logo1.png" alt="">'
     }
 })
