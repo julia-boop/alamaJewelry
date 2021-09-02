930a46b906cf130241595636894f431c461d7a42
julia-boop <crdr.jc@gmail.com>
2021-09-02 12:42:32 -0300

    cambios a shop internacional

--- public/js/home.js
+++ public/js/home.js
@@ -4,13 +4,9 @@ window.addEventListener('load', () => {
     let logo = document.querySelector('.logo')
 
     if(window.innerWidth <= 760){
-        arg.innerHTML = 'shop arg'
-        eu.innerHTML = 'shop eu '
         logo.innerHTML = '<img src="/img/logoAlta.png" alt="">'
     }
     if(window.innerWidth > 760) {
-        arg.innerHTML = 'shop argentina'
-        eu.innerHTML = 'shop europe '
         logo.innerHTML = '<img src="/img/logo1.png" alt="">'
     }
 })
