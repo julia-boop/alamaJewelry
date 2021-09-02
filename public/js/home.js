0234c56272cf170a39fdcd3b23bdee0326b16865
julia-boop <crdr.jc@gmail.com>
2021-09-02 12:53:48 -0300

    cambios del header

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', () => {
     let arg = document.querySelector('#header-arg')
-    let eu =  document.querySelector('#header-eu')
+    let int =  document.querySelector('#header-int')
     let logo = document.querySelector('.logo')
 
     if(window.innerWidth <= 760){
+        arg.innerHTML = 'Shop <br> Argentina'
+        int.innerHTML = 'Shop <br> Internacional'
         logo.innerHTML = '<img src="/img/logoAlta.png" alt="">'
     }
     if(window.innerWidth > 760) {
+        arg.innerHTML = 'Shop Argentina'
+        int.innerHTML = 'Shop Internacional'
         logo.innerHTML = '<img src="/img/logo1.png" alt="">'
     }
 })
